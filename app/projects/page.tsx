1b1796ab2f1826598216f965f951039eb8cabf71
noogler-eng <sharadpoddar10102003@gmail.com>
2025-05-16 22:16:53 +0530

    projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,16 +1,26 @@
+"use client";
 import Link from "next/link";
 import type { Metadata } from "next";
 import { projects } from "./project-data";
+import { motion } from "framer-motion";
 
-export const metadata: Metadata = {
-  title: "Projects",
-  description: "Nextfolio Projects",
-};
 
 export default function Projects() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-medium">Projects</h1>
+      <motion.h1
+        className="mb-8 text-2xl font-medium bg-gradient-to-r from-pink-500 via-yellow-500 to-purple-500 bg-[length:200%] bg-clip-text text-transparent"
+        animate={{
+          backgroundPosition: ["0% 50%", "100% 50%", "0% 50%"],
+        }}
+        transition={{
+          duration: 6,
+          ease: "linear",
+          repeat: Infinity,
+        }}
+      >
+        Projects
+      </motion.h1>
       <div>
         {projects.map((project, index) => (
           <Link
@@ -20,8 +30,11 @@ export default function Projects() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center space-y-1 sm:space-y-0 sm:space-x-2">
-              <h2 className="text-black dark:text-white">{project.title}</h2>
+            <div className="w-full flex flex-col justify-between gap-2">
+              <div className="w-full flex">
+                <h2 className="text-black dark:text-white">{project.title}</h2>
+                <p className="text-black dark:text-white">{project.time}</p>
+              </div>
               <p className="text-neutral-600 dark:text-neutral-400">
                 {project.description}
               </p>
--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -1,21 +1,45 @@
 export interface Project {
   title: string;
-  year: number;
+  time: string;
   description: string;
   url: string;
 }
 
 export const projects: Project[] = [
   {
-    title: "Mithril AI",
-    year: 2024,
-    description: "Open science AI resarch lab",
-    url: "https://github.com/mithrilai",
+    title: "Music Labs",
+    time: "1 month",
+    description: `A music player app built with React and Tailwind 
+    CSS and in backend there is websockets server and express js, 
+    postgres for database, it is an async music system in two users 
+    from different part of world can listen same music at a time, 
+    it also has a features like music queues, priority, vote and down 
+    votes host side pause etc`,
+    url: "https://github.com/noogler-eng/Music-Labs",
   },
   {
-    title: "OpenDeepLearning",
-    year: 2023,
-    description: "Open source AI education resources",
-    url: "https://opendeeplearning.xyz/",
+    title: "Paymee",
+    time: "10 days",
+    description: `A web based payment system, where user can pay and 
+    receive money, it enables dbms concepts like trasaction, rollback, 
+    commit etc, transactions history is maintained in the database,`,
+    url: "https://github.com/noogler-eng/frontend-paymee",
+  },
+  {
+    title: "Contest Tracker",
+    time: "2 days",
+    description: `A web based contest tracker, where user can track the
+    latest upcomming contests, past contests also, there is automatic uploads
+    of youtube contest solution video on this platform (automation using google youtube apis)
+    and there is feature of user bookmarking the contest. it is deployed on render`,
+    url: "https://github.com/noogler-eng/assignment",
+  },
+  {
+    title: "Ai Based Notes Summarizer",
+    time: "2 days",
+    description: `A web based notes summarizer, where user can write their notes and 
+    on time of revising the old notes they can summarize it using AI (gemini ai has been
+    used for this purpose), supabase for user authentication and database,`,
+    url: "https://github.com/noogler-eng/1811-labs-assignment",
   },
 ];
